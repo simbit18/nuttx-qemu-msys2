c3c729258b21ae15b0f623e3ac6a020e91fa5525
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-08-23 16:52:47 +0200

    Update Config.mk

--- tools/esp32/Config.mk
+++ tools/esp32/Config.mk
@@ -272,9 +272,9 @@ endef
 else
 define MKIMAGE
 	$(Q) echo "MKIMAGE: ESP32 binary"
-	$(Q) if ! esptool.py version 1>/dev/null 2>&1; then \
+	$(Q) if ! $(ESPTOOL_FILE) version 1>/dev/null 2>&1; then \
 		echo ""; \
-		echo "esptool.py not found.  Please run: \"pip install esptool==4.8.dev4\""; \
+		echo "$(ESPTOOL_FILE) not found.  Please run: \"pip install esptool==4.8.dev4\""; \
 		echo ""; \
 		echo "Run make again to create the nuttx.bin image."; \
 		exit 1; \
